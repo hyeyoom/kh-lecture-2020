39d8574046593bf6d5d809fb2615dd80afb86087
hyeyoom <neoul_chw@icloud.com>
2020-11-13 01:23:44 +0900

    soml

--- frontend/javascript/app.js
+++ frontend/javascript/app.js
@@ -1,6 +1,11 @@
-const message = (function() {
-    const private = '있어보이는데이터'
-    return private
-})()
+const data = [
+    { name: 'Conan Gray', mail: 'adsf@adsf.com' },
+    { name: 'Chiho Won', mail: 'a@b.com' },
+    { name: 'Stan', mail: 'b@b.com' },
+]
 
-console.log(message)
+const mails = data.map(x => x.mail)
+console.log(mails)
+
+const name = data.map(x => x.name).filter(name => name.startsWith('C'))
+console.log(name)
