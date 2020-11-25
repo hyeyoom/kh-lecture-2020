fc1930033a79c1b584e626b047999f8aec24ab1e
hyeyoom <neoul_chw@icloud.com>
2020-11-25 23:14:57 +0900

    fixed untracekd files

---
+++ server/code/mybatis/src/main/java/com/github/hyeyoom/springbootonintellij/domain/User.java
@@ -0,0 +1,13 @@
+package com.github.hyeyoom.springbootonintellij.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String email;
+}
